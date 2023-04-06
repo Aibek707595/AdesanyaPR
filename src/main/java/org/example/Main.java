76eab8ab770a7cc43f1c6e5aa825abe8ab34e94c
Aibek707595 <aibek.amangeldiyev@mail.ru>
2023-04-07 02:18:27 +0500

    midleweight champion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("and New Champion");
+        System.out.println("and New Champion of the world");
     }
 }
